d2025f38ce1fe7c7d9e4d12e3127c443d0feff63
Diphany <diphany.chea@gmail.com>
2021-09-10 02:52:44 -0400

    Files correction

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -11,7 +11,8 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(640, 480, 0);
+  background(255, 218, 243);
 }
 
 // draw()
