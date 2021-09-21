4cc6003b25cf2bf2e39f2fb33b5bcd4410e2809b
Diphany <diphany.chea@gmail.com>
2021-09-21 14:11:44 -0400

    E1: Started exercise 1

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -11,8 +11,11 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(640, 480, 0);
+  createCanvas(640, 480);
   background(255, 218, 243);
+  noStroke();
+  fill(190, 167, 252);
+  ellipse(320, 360, 240, 240);
 }
 
 // draw()
