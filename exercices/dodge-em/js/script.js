373429541faad06e21a0e6f60c078f4f978369e1
Diphany <diphany.chea@gmail.com>
2021-09-29 03:03:51 -0400

    Aesthetic background elements added and title corrected

--- exercices/dodge-em/js/script.js
+++ exercices/dodge-em/js/script.js
@@ -2,13 +2,14 @@
 Activity #4 - Dodging COVID-19
 Diphany Chea
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Sea scene where you control a small bubble and you must not let the bigger
+bubble engulf you!
 */
 
 "use strict";
 
-let covid19 = {
+// Big bubble
+let enemyBubble = {
   x: 0,
   y: 250,
   size: 100,
@@ -24,6 +25,7 @@ let covid19 = {
   }
 };
 
+// Small bubble
 let user = {
   x: 250,
   y: 250,
@@ -37,50 +39,60 @@ let user = {
   maxSpeed: 20
 };
 
-let numStatic = 1000;
+// Bubble in the background
+let numStatic = 10;
+
+// Fishes in the background
+let blueFish, orangeFish = {
+  x: 0,
+  y: 0,
+  speed: 5
+};
+
 
 /**
-Description of preload
+Preloading images of fish for aesthetic purposes :>
 */
 function preload() {
-
+  blueFish = loadImage('assets/images/blueFish.png');
+  orangeFish = loadImage('assets/images/orangeFish.png');
 }
 
 
 /**
-Description of setup
+Enemy spawn
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  covid19.y = random(0, height);
-  covid19.vx = covid19.speed;
-
-  // noCursor();
+  enemyBubble.y = random(0, height);
+  enemyBubble.vx = enemyBubble.speed;
 }
 
 
 /**
-Description of draw()
+Create the dynamic and static elements in the scene
 */
 function draw() {
-  background(0);
+  background(20, 50, 99);
+  imageMode(CENTER);
 
-  // Display static
+  // Display bubbles in the background
   for (let i = 0; i < numStatic; i++) {
     let x = random(0, width);
     let y = random(0, height);
-    stroke(255);
+    stroke(58, 139, 201);
+    strokeWeight(20);
     point(x, y);
   }
 
-  // Covid 19 movement
-  covid19.x = covid19.x + covid19.vx;
-  covid19.y = covid19.y + covid19.vy;
+  // Bad bubble movement
+  enemyBubble.x = enemyBubble.x + enemyBubble.vx;
+  enemyBubble.y = enemyBubble.y + enemyBubble.vy;
 
-  if (covid19.x > width) {
-    covid19.x = 0;
-    covid19.y = random(0, height);
+  if (enemyBubble.x > width) {
+    enemyBubble.x = 0;
+    enemyBubble.y = random(0, height);
   }
 
   // User movement + acceleration effect when moving
@@ -106,30 +118,46 @@ function draw() {
   user.x = user.x + user.vx;
   user.y = user.y + user.vy;
 
-  // Check for catching covid19
-  let d = dist(user.x, user.y, covid19.x, covid19.y);
-  if (d < covid19.size/2 + user.size/2) {
+  // Check for catching enemyBubble
+  let d = dist(user.x, user.y, enemyBubble.x, enemyBubble.y);
+  if (d < enemyBubble.size/2 + user.size/2) {
     noLoop();
   }
-  print(d);
+  // print(d);
 
   /*
-    Increase covid19 size if it's getting close to the user and decrease if
+    Increase enemyBubble size if it's getting close to the user and decrease if
     it's getting further away from the user
   */
-
-  if (d < covid19.x/5 + user.x/5) {
-      covid19.size = covid19.size + covid19.sizeScale;
-      covid19.size = constrain(covid19.size, 100, covid19.maxScale);
+  if (d < enemyBubble.x/5 + user.x/5) {
+      enemyBubble.size = enemyBubble.size + enemyBubble.sizeScale;
+      enemyBubble.size = constrain(enemyBubble.size, 100, enemyBubble.maxScale);
   }
   else {
-    covid19.size = covid19.size - covid19.sizeScale;
-    covid19.size = constrain(covid19.size, 100, covid19.maxScale);
+    enemyBubble.size = enemyBubble.size - enemyBubble.sizeScale;
+    enemyBubble.size = constrain(enemyBubble.size, 100, enemyBubble.maxScale);
   }
 
-  // Display covid 19
-  fill(covid19.fill.r, covid19.fill.g, covid19.b);
-  ellipse(covid19.x, covid19.y, covid19.size);
+  // Fish movement
+  // blueFish.x = blueFish.x + blueFish.speed;
+  // orangeFish.x = orangeFish.x + orangeFish.speed;
+
+  /*
+    Bunch of fishes in the background for decorations
+    I don't know how to make images move so :<
+  */
+  image(blueFish, 300, 300, 300, 200);
+  image(blueFish, 600, 800, 300, 200);
+  image(blueFish, 1000, 100, 150, 100);
+  image(blueFish, 1600, 500, 100, 75);
+  image(orangeFish, 300, 700, 100, 75);
+  image(orangeFish, 800, 400, 150, 100);
+  image(orangeFish, 1400, 800, 300, 200);
+  image(orangeFish, 1800, 200, 300, 200);
+
+  // Display enemy bubble
+  fill(enemyBubble.fill.r, enemyBubble.fill.g, enemyBubble.b);
+  ellipse(enemyBubble.x, enemyBubble.y, enemyBubble.size);
 
   // Display user
   fill(user.fill);
